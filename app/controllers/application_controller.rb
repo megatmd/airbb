759cacd09d39a5c0173ad395c3ed76957eee98a4
megatmd <megatmd@Megats-MacBook-Pro.local>
2017-06-06 21:34:04 +0800

    clearance working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
   protect_from_forgery with: :exception
 end
+
+# 
+# class ArticlesController < ApplicationController
+#   before_action :require_login
+# end
